7dd473a6ab0d068cd7af2b68969f8509db7e0adf
lamymay <lamymay@outlook.com>
2019-09-15 20:53:32 +0800

    nginx  for zero peizhi

---
+++ nginx1_14_2/html/precache-manifest.a36c97f2e1bc2437ba00b30f81c1d0ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca9c3deef18d2ee2b8458e0dbc30494",
+    "url": "/index.html"
+  },
+  {
+    "revision": "804d2d728324108c4e54",
+    "url": "/static/css/2.33bbdc9a.chunk.css"
+  },
+  {
+    "revision": "8d70fcc8b6b9c9369860",
+    "url": "/static/css/main.ac0cb186.chunk.css"
+  },
+  {
+    "revision": "804d2d728324108c4e54",
+    "url": "/static/js/2.2d958f1e.chunk.js"
+  },
+  {
+    "revision": "8d70fcc8b6b9c9369860",
+    "url": "/static/js/main.e23cfdee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ nginx1_14_2/run_nginx.bat
@@ -0,0 +1,9 @@
+@echo off
+REM 注释 后续命令使用的是：UTF-8编码
+chcp 65001
+echo 中文测试--启动nginx
+T:\P\nginx\nginx1_14_2\nginx.exe
+
+
+echo \n  按任意键退出
+pause
